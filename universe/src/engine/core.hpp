d4019895ce8a9986020db08d44e118c3d7452d64
Kefsner <kesleyraimundo@gmail.com>
2024-12-03 13:01:43 -0300

    add debug/release flags to cmake

--- universe/src/engine/core.hpp
+++ universe/src/engine/core.hpp
@@ -3,18 +3,23 @@
 #include <memory>
 #include <csignal>
 
-// Cross-platform debug break function
-inline void trigger_debug_break() {
-#if defined(_WIN32)
-    __debugbreak();
-#elif defined(__linux__)
-    raise(SIGTRAP);
-#endif
-}
 
-// Assert Macros
-#define UE_ASSERT(x, ...) { if(!(x)) { UE_ERROR("Assertion Failed: {0}", __VA_ARGS__); trigger_debug_break(); } }
-#define UE_CORE_ASSERT(x, ...) { if(!(x)) { UE_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); trigger_debug_break(); } }
+#if defined(UE_DEBUG)
+    // Cross-platform debug break function
+    inline void trigger_debug_break() {
+    #if defined(_WIN32)
+        __debugbreak();
+    #elif defined(__linux__)
+        raise(SIGTRAP);
+    #endif
+    }
+    // Assert Macros
+    #define UE_ASSERT(x, ...) { if(!(x)) { UE_ERROR("Assertion Failed: {0}", __VA_ARGS__); trigger_debug_break(); } }
+    #define UE_CORE_ASSERT(x, ...) { if(!(x)) { UE_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); trigger_debug_break(); } }
+#else 
+    #define UE_ASSERT(x, ...)
+    #define UE_CORE_ASSERT(x, ...)
+#endif
 
 // Bind Macro
 #define UE_BIND_EVENT_FN(fn) std::bind(&fn, this, std::placeholders::_1)
