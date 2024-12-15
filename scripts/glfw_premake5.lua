c4d80c3617735d4741163acff0d5662f3a139621
Kefsner <kesleyraimundo@gmail.com>
2024-12-15 18:13:28 -0300

    update scripts/glfw_premake5.lua

--- scripts/glfw_premake5.lua
+++ scripts/glfw_premake5.lua
@@ -9,25 +9,24 @@ project "GLFW"
    objdir ("%{wks.location}/build/obj/%{cfg.buildcfg}/%{prj.name}")
 
    files {
-      -- Headers
       "%{prj.location}/include/GLFW/glfw3.h",
       "%{prj.location}/include/GLFW/glfw3native.h",
-
-      -- Core
-      "%{prj.location}/src/context.c",
       "%{prj.location}/src/init.c",
+      "%{prj.location}/src/window.c",
+      "%{prj.location}/src/context.c",
       "%{prj.location}/src/input.c",
       "%{prj.location}/src/monitor.c",
-      "%{prj.location}/src/window.c",
       "%{prj.location}/src/vulkan.c",
-      -- Careful from here
       "%{prj.location}/src/platform.c",
-
+      "%{prj.location}/src/null_init.c",
+      "%{prj.location}/src/null_monitor.c",
+      "%{prj.location}/src/osmesa_context.c",
+      "%{prj.location}/src/egl_context.c",
+      "%{prj.location}/src/null_window.c",
+      "%{prj.location}/src/null_joystick.c",
    }
 
-   filter "system:windows"
-      systemversion "latest"
-      
+   filter "system:windows"     
       files {
          "%{prj.location}/src/win32_thread.c",
          "%{prj.location}/src/win32_time.c",
@@ -37,13 +36,6 @@ project "GLFW"
          "%{prj.location}/src/win32_joystick.c",
          "%{prj.location}/src/wgl_context.c",
          "%{prj.location}/src/win32_window.c",
-         -- Why null stuff?
-         "%{prj.location}/src/null_init.c",
-         "%{prj.location}/src/null_monitor.c",
-         "%{prj.location}/src/osmesa_context.c",
-         "%{prj.location}/src/egl_context.c",
-         "%{prj.location}/src/null_window.c",
-         "%{prj.location}/src/null_joystick.c",
       }
 
       links {
